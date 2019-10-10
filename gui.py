0ce6084f4caa5168a3fa16e4b3a858144d5f12c1
James Sofios <jamessofios@gmail.com>
2019-10-09 21:33:05 -0500

    updated .gitignore, renamed simpledes, added textboxes to the gui

--- gui.py
+++ gui.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 #possible gui frontend
 #looking into TKinter
-import sdes
+from simpledes import *
 
 #download python3-tk on debian to use
-import tkinter
+from tkinter import *
 
-top = tkinter.Tk()
+top = Tk()
+# Code to add widgets will go here...
 
-top.title("SDES Encrypt/Decrypt")
-top.label(top, text="Hello")
-top.geometry('500x250')
-top.Button(window, text="Click Me", bg="orange", fg="red")
+#label and entry box for the 8 bit bitstring
+l1 = Label(top, text="Bitstring:")
+l1.grid(column=0,row=0)
+
+e1 = Entry(top, bd = 5)
+e1.grid(column=1,row=0)
+
+#label and entrybox for the 10 bit key bitstring
+l2 = Label(top, text="Key:")
+l2.grid(column=0,row=1)
+
+e2 = Entry(top, bd =5)
+e2.grid(column=1,row=1)
 
 top.mainloop()
