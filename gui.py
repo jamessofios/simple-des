b34e011999b460e8001b3f988452e635e628926a
James Sofios <jamessofios@gmail.com>
2019-10-09 22:13:44 -0500

    improved the gui

--- gui.py
+++ gui.py
@@ -10,6 +10,7 @@ for python3 on debian 10(Stable)
 from tkinter import *
 
 top = Tk()
+top.title("simpledes")
 # Code to add widgets will go here...
 
 #label and entry box for the 8 bit bitstring
@@ -20,16 +21,24 @@ e1 = Entry(top, bd = 5)
 e1.grid(column=1,row=0)
 
 #label and entrybox for the 10 bit key bitstring
-l2 = Label(top, text="Key:")
-l2.grid(column=0,row=1)
+l2 = Label(top, text= "Key:")
+l2.grid(column = 0, row = 1)
 
-e2 = Entry(top, bd =5)
-e2.grid(column=1,row=1)
+e2 = Entry(top, bd = 5)
+e2.grid(column = 1, row = 1)
 
-r1 = Radiobutton(top, text="Encrypt", value = 1)
+l3 = Label(top, text = "Result:")
+l3.grid(column = 0, row = 2)
+
+t1 = Label(top, text = " ")
+t1.grid(column = 1, row = 2)
+
+rbVar = IntVar()
+
+r1 = Radiobutton(top, text="Encrypt", variable = rbVar, value = 1)
 r1.grid(column=1,row=3)
 
-r2 = Radiobutton(top, text="Decrypt", value = 2)
+r2 = Radiobutton(top, text="Decrypt", variable = rbVar, value = 2)
 r2.grid(column=1,row=4)
 
 b1 = Button(top, text = "Go")
